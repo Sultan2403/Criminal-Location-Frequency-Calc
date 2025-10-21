f7a5eb190d98b297bacd56824501290c253105e5
Sultan2403 <sultanremilekun@gmail.com>
2025-10-21 17:54:20 +0100

    nth

--- app.js
+++ app.js
@@ -1,85 +1,115 @@
 // Array of all logged locations
 
-let criminalLocations = ["market", "church", "office", "mall", "gym", "bank", "saloon", "eatery", "spa", "club", "office", "airport", "gym", "eatery", "market", "office", "mall", "eatery", "spa", "market", "eatery", "saloon", "church", "office", "spa", "club", "office", "office", "mall", "eatery", "gym", "bank", "saloon", "airport", "gym", "eatery"]
+let criminalLocations = [
+  "market",
+  "church",
+  "office",
+  "mall",
+  "gym",
+  "bank",
+  "saloon",
+  "eatery",
+  "spa",
+  "club",
+  "office",
+  "airport",
+  "gym",
+  "eatery",
+  "market",
+  "office",
+  "mall",
+  "eatery",
+  "spa",
+  "market",
+  "eatery",
+  "saloon",
+  "church",
+  "office",
+  "spa",
+  "club",
+  "office",
+  "office",
+  "mall",
+  "eatery",
+  "gym",
+  "bank",
+  "saloon",
+  "airport",
+  "gym",
+  "eatery",
+];
 
 // Object to help handle info easily
 
-let criminalLocationsFrequency = {}
+let criminalLocationsFrequency = {};
 
 // DOM Stuff
 
-const btn = document.getElementById("btn")
-
-
+const btn = document.getElementById("btn");
 
 // Add locations from user input
 
-
-
-const display = document.getElementById("display-area")
+const display = document.getElementById("display-area");
 function createNewLocation() {
-    const newLocation = document.getElementById("user-input").value.toLowerCase();
+  const newLocation = document.getElementById("user-input").value.toLowerCase();
 
-    if (newLocation.trim().length < 3) {
-        display.textContent = `⚠️ Please enter a valid location of a minimum of three characters!`;
-        setTimeout(() => {
-            display.textContent = ''
-        }, 2500);
-        return;
-    } else {
-        criminalLocations.push(newLocation);
-        analyzeCriminalLocations()
-    }
+  if (newLocation.trim().length < 3) {
+    display.textContent = `⚠️ Please enter a valid location of a minimum of three characters!`;
+    setTimeout(() => {
+      display.textContent = "";
+    }, 2500);
+    return;
+  } else {
+    criminalLocations.push(newLocation);
+    analyzeCriminalLocations();
+  }
 
-
-    console.log(criminalLocations);
+  console.log(criminalLocations);
 }
 
 btn.addEventListener("click", createNewLocation);
 
-
 // Analyze criminal locations!
 
-
-
 function analyzeCriminalLocations() {
-    criminalLocationsFrequency = {};
-    for (let i = 0; i < criminalLocations.length; i++) {
-        let currentitem = criminalLocations[i];
+  criminalLocationsFrequency = {};
+  for (let i = 0; i < criminalLocations.length; i++) {
+    let currentitem = criminalLocations[i];
 
-        // If Item doesn't exist ......
+    // If Item doesn't exist ......
 
-        if (!(currentitem in criminalLocationsFrequency)) {
-            criminalLocationsFrequency[currentitem] = {}
-            criminalLocationsFrequency[currentitem].name_of_place = currentitem
-            criminalLocationsFrequency[currentitem].count = 1;
-        }
-
-        // If item does exist
-
-        else {
-            criminalLocationsFrequency[currentitem].count++
-        }
+    if (!(currentitem in criminalLocationsFrequency)) {
+      criminalLocationsFrequency[currentitem] = {};
+      criminalLocationsFrequency[currentitem].name_of_place = currentitem;
+      criminalLocationsFrequency[currentitem].count = 1;
     }
 
-    let sortedCriminalLocationsFrequency = Object.values(criminalLocationsFrequency).sort((a, b) => b.count - a.count)
-
-    // To output to the display 
-
-    display.innerHTML = "<p>The criminal has been to the following places</p>"
-    for (let i = 0; i < sortedCriminalLocationsFrequency.length; i++) {
-        let currentKey = sortedCriminalLocationsFrequency[i]
-        const locationDisplay = document.createElement("p")
-
-        // Converts first letter to Uppercase...
-
-        locationDisplay.textContent = `${currentKey.name_of_place.charAt(0).toUpperCase() + currentKey.name_of_place.slice(1)}: ${currentKey.count} time(s)`
-        display.appendChild(locationDisplay)  //Tells JS to add as a child to the parent specified in the parantheses 
+    // If item does exist
+    else {
+      criminalLocationsFrequency[currentitem].count++;
     }
+  }
 
-    // Just for logging so I know what I'm doing :) 
-    console.log(criminalLocationsFrequency)
+  let sortedCriminalLocationsFrequency = Object.values(
+    criminalLocationsFrequency
+  ).sort((a, b) => b.count - a.count);
 
+  // To output to the display
 
+  display.innerHTML = "<p>The criminal has been to the following places</p>";
+  for (let i = 0; i < sortedCriminalLocationsFrequency.length; i++) {
+    let currentKey = sortedCriminalLocationsFrequency[i];
+    const locationDisplay = document.createElement("p");
 
+    // Converts first letter to Uppercase...
+
+    locationDisplay.textContent = `${
+      currentKey.name_of_place.charAt(0).toUpperCase() +
+      currentKey.name_of_place.slice(1)
+    }: ${currentKey.count} time(s)`;
+    display.appendChild(locationDisplay); //Tells JS to add as a child to the parent specified in the parantheses
+  }
+
+  // Just for logging so I know what I'm doing :)
+  console.log(criminalLocationsFrequency);
 }
